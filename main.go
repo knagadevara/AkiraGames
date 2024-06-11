6437074139b3063544d60480e88b708e6269aa2a
knagadevara <vnsk.1991@gmail.com>
2024-06-11 19:52:26 +0530

    Del rev

--- main.go
+++ main.go
@@ -5,29 +5,22 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/knagadevara/AkiraGames/GameOn/Blanks"
 	Hangman "github.com/knagadevara/AkiraGames/GameOn/Hangman"
-	staticfiles "github.com/knagadevara/AkiraGames/StaticFiles"
 	"github.com/knagadevara/AkiraGames/utl"
 )
 
 func displayGames() {
 	fmt.Println("Please Select a Game")
 	fmt.Println("1. Hangman")
-	fmt.Println("2. Blanks")
 }
 
 func main() {
 	displayGames()
 	inpRdr := bufio.NewReader(os.Stdin)
 	enterSelection := string(utl.GetRune()(inpRdr))
-	resp := staticfiles.BuildData()
 
 	switch enterSelection {
 	case "1":
-		bl := Blanks.BlanksPlayer{}
-		bl.Start(resp)
-	case "2":
 		cl := Hangman.CliffhangerPlayerData{}
 		cl.Start()
 	default:
