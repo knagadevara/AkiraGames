dac9e637688464d35c7d82784e90b4e9ed621cc7
knagadevara <vnsk.1991@gmail.com>
2024-06-13 17:36:59 +0530

    go fix end

--- flow.go
+++ flow.go
@@ -29,14 +29,14 @@ func displayGames() {
 	fmt.Println("1. Hangman")
 }
 
-func main() {
+func PopGame() {
 	displayGames()
 	inpRdr := bufio.NewReader(os.Stdin)
 	enterSelection := string(utl.GetRune()(inpRdr))
 
 	switch enterSelection {
 	case "1":
-		cl := Hangman.CliffhangerPlayerData{}
+		cl := CliffhangerPlayerData{}
 		cl.Start()
 	default:
 		fmt.Printf("Please enter a valid number!!\n%v is not accepted\n", enterSelection)
---
+++ main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/knagadevara/AkiraGames"
+
+func main() {
+	AkiraGames.PopGame()
+}
